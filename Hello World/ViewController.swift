dbb182b67e8d34052b6f242653edb2fb4a2c2992
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-01-26 14:17:13 -0700

    Stretch #6:

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mindCenterLabel: UILabel!
+    
+    var mindLabel = "Mind Center"
+    
+    @IBOutlet weak var pressButton: UIButton!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        mindCenterLabel.text =  mindLabel
+        
+        pressButton.backgroundColor = .yellow    }
 
 
 }
