87491ca36b0661acc96f494ce92a1f8a5ef4fd61
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-01-27 14:14:27 -0700

    Done Stretch #7

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -15,14 +15,24 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var pressButton: UIButton!
     
-   
+    @IBOutlet weak var cat3: UIImageView!
+    
+    @IBOutlet weak var funText: UITextField!
+    
     override func viewDidLoad() {
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         mindCenterLabel.text =  mindLabel
         
-        pressButton.backgroundColor = .yellow    }
+        pressButton.backgroundColor = .yellow
+        
+        cat3.image = UIImage (named: "cat 3")
 
+       var funText = "What Fun"
+        
+    }
 
+ 
 }
 
